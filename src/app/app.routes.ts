fd189edfde9ab9e2486cf2d5d3093acf49e171cf
rdat5 <ray.datuin@gmail.com>
2024-07-18 20:12:14 -0700

    Create basic structure for site.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', title:"Home", component: HomeComponent },
+    { path: 'shop', title:"Shop", component: ShopComponent },
+    { path: 'cart', title:"Shopping Cart", component: CartComponent }
+];
