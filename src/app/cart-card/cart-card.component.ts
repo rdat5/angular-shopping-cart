217a643ef648f1ec675997c1fa0de08e7fd6eb89
rdat5 <ray.datuin@gmail.com>
2024-07-28 18:37:27 -0700

    Add basic layout for cart page.

--- src/app/cart-card/cart-card.component.ts
+++ src/app/cart-card/cart-card.component.ts
@@ -5,7 +5,31 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>cart-card works!</p>
+    <div class="box">
+      <article class="media">
+        <figure class="media-left">
+          <div class="image is-96x96">
+            <img src="https://bulma.io/assets/images/placeholders/128x128.png" alt="Image" />
+          </div>
+        </figure>
+        <div class="media-content mr-6">
+          <div class="level">
+            <div class="level-left">
+              <div class="container">
+                <p class="title">Blah</p>
+                <p class="subtitle">Quantity: 1</p>
+              </div>
+            </div>
+            <div class="level-right">
+              <div class="container">
+                  <p class="subtitle">Blah</p>
+                  <button class="button is-danger">Remove From Cart</button>
+                </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
   `
 })
 export class CartCardComponent {
